13b9e775b26a89efb2f27ea33d00eb4152c485eb
Ned flanders <amcdaugale05@gmail.com>
2023-12-17 19:20:17 -0700

    Checking in work

---
+++ Module1_CriticalThinking/resturauntInfo.java
@@ -0,0 +1,18 @@
+/**
+ * @author Alec Mcdaugale
+ * December 17,2023
+ **/
+
+class  ResturauntInfo {
+    public static void main(String[] args) {
+        final String resturauntName = "McDonalds";
+	final String businessName = "McDonalds Inc.";
+        final String city = "Denver";
+        final String state = "Colorado";
+        final int zipCode = 80236;    
+        System.out.println(resturauntName);
+        System.out.println(businessName);
+        System.out.println(city + ", " +  state + " " + zipCode);
+        System.exit(0);
+    }
+}
